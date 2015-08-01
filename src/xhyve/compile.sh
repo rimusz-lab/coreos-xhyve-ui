e6d4dfc9d87badc275b3ff8a1dbac4ecb4b6cf00
rimusz <rmocius@gmail.com>
2015-08-01 11:25:35 +0100

    no message

--- src/xhyve/compile.sh
+++ src/xhyve/compile.sh
@@ -5,8 +5,8 @@
 git clone https://github.com/mist64/xhyve
 cd xhyve
 make
-sudo chown root build/xhyve
-sudo chmod +s build/xhyve
+#sudo chown root build/xhyve
+#sudo chmod +s build/xhyve
 
 cp -f build/xhyve ../../bin
 cd ..
